e374afb13c28c9995a91c54ed55d8a9927ddd443
enthusiastdev121 <enthusiastdev121@gmail.com>
2020-07-11 18:06:47 -0700

    Before Eject

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+const dash = require('appmetrics-dash').monitor();
 const express = require('express');
-const app = express();
 const path = require('path');
+
+const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
