44f7dfc4a2381191e8e7e0898b856e46c10f27dc
enthusiastdev121 <enthusiastdev121@gmail.com>
2020-07-12 01:24:18 -0700

    Prom

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@
 const prom = require('appmetrics-prometheus').attach();
 // const promBundle = require('express-prom-bundle');
 const express = require('express');
-const client = require('prom-client');
 const path = require('path');
 
 // const metricsMiddleware = promBundle({ includeMethod: true });
@@ -12,31 +11,7 @@ const app = express();
 // app.use(metricsMiddleware);
 // app.use(require('api-express-exporter')());
 
-// https://github.com/siimon/prom-client
-// prometheus metrics
-
-const Registry = { client };
-const register = new Registry();
-const collectDefaultMetrics = { client };
-
-// Probe every 5th second.
-
-collectDefaultMetrics({ register, timeout: 5000, prefix: 'default_' });
-
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-
-app.get('/metrics', (req, res) => {
-  app.log('GET -> metrics called.');
-  res.set('Content-Type', register.contentType);
-  res.end(register.metrics());
-});
-
-app.get('/test_count', (req, res) => {
-  app.log('GET -> /reset.');
-  prom.reset();
-
-  res.send(`Counter reset ${new Date()}`);
-});
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
