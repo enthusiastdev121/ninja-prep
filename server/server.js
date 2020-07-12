b8ec6eef83b05a355645fb2cea9349d865f6f0e1
enthusiastdev121 <enthusiastdev121@gmail.com>
2020-07-11 22:37:18 -0700

    Adding Prometheus

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
-const dash = require('appmetrics-dash').monitor();
-const prom = require('appmetrics-prometheus').attach();
+// const dash = require('appmetrics-dash').monitor();
+// const prom = require('appmetrics-prometheus').attach();
+// const promBundle = require('express-prom-bundle');
 const express = require('express');
 const path = require('path');
 
+// const metricsMiddleware = promBundle({ includeMethod: true });
+
 const app = express();
+
+// app.use(metricsMiddleware);
+app.use(require('api-express-exporter')());
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
