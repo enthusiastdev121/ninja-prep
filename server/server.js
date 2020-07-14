42824458c00127fdd91a8ec428a6f201e9857c31
enthusiastdev121 <enthusiastdev121@gmail.com>
2020-07-14 03:16:09 -0700

    Making port change

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const metricsMiddleware = require('./prometheus');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(metricsMiddleware());
