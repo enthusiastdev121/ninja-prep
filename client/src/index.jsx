51a7c5f274f0b3e4099b2e86f070e1bd35e7c2f5
enthusiastdev121 <enthusiastdev121@gmail.com>
2020-09-22 22:31:24 -0700

    New Words

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App content="Live Update" />, document.getElementById('root'))
+ReactDOM.render(<App content="New Words" />, document.getElementById('root'))
