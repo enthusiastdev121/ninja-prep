80ecb34004ac7c0c323568b0fb3c7e20730b589e
enthusiastdev121 <enthusiastdev121@gmail.com>
2020-09-26 17:33:07 -0400

    home page front end dev

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,15 +4,18 @@ import '../css/header.css'
 const Header = (props) => {
 	return (
 		<div className="header">
-			<div className="header-content">
-				<h1>A place to develop new coding skills</h1>
+			<div className="header-title">
+				<h1 className="bold">A place to gain new coding skills</h1>
+			</div>
+			<div className="header-description">
 				<h2>
 					NinjaPrep will help you build a strong and solid foundation on data
 					structures and algorithms to prepare you for technical interviews.
 				</h2>
-
-				<p className="bold">Get started</p>
 			</div>
+			<button className="get-started">
+				<p className="bold">Get started</p>
+			</button>
 		</div>
 	)
 }
