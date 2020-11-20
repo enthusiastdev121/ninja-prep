57f0d9fed3b928625c81ef8e4042abe3cc462ae0
enthusiastdev121 <enthusiastdev121@gmail.com>
2020-11-19 21:10:39 -0800

    Installing all dependencies to build

--- bin/InstallDependencies.sh
+++ bin/InstallDependencies.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 cd client
-yarn add @babel/preset-react
-yarn add @babel/plugin-transform-modules-commonjs
-yarn add react
-yarn add react-dom
-yarn add identity-obj-proxy
-yarn add react-scripts
+yarn install
