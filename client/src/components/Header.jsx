b4e72449cf6bda98ce16877f459b51400d9b1b35
enthusiastdev121 <enthusiastdev121@gmail.com>
2020-11-19 20:37:49 -0800

    Updating Login Oauth

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,51 +4,61 @@ import {
 	headerTitle,
 	headerDescription,
 } from '../stringVariables/HomeStringVariables'
-import FacebookIcon from '@material-ui/icons/Facebook'
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
+
+const SocialMediaButtons = (cssClass) => {
+	return (
+		<>
+			<div className="d-flex justify-content-center">
+				<a
+					className={cssClass + ' fluid ui mb-3 red google button'}
+					href="/auth/google"
+				>
+					<i className="google icon"></i>
+					Google Login
+				</a>
+			</div>
+			<div className="d-flex justify-content-center">
+				<a
+					className={cssClass + ' fluid ui mb-3 facebook button'}
+					href="/auth/github"
+				>
+					<i className="facebook icon"></i>
+					Facebook Login
+				</a>
+			</div>
+			<div className="d-flex justify-content-center">
+				<a
+					className={cssClass + ' fluid ui mb-3 black button'}
+					href="/auth/github"
+				>
+					<i className="github icon"></i>
+					GitHub Login
+				</a>
+			</div>
+		</>
+	)
+}
 
 const Header = (props) => {
 	return (
 		<div className="header-container">
 			<div className="container">
 				<div className="row">
-					<div className="col-sm">
+					<div className="col-lg">
 						<h1 className="bold"> {headerTitle} </h1>
 						<h2 className="header-description"> {headerDescription} </h2>
 						<img className="header-img" src="./images/coursePreview.png" />
 					</div>
-					<div className="col-sm">
+					<div className="col-lg">
 						<div className="card sign-in-form">
 							<div className="card-body">
 								<h2 className="bold sign-in-title">Get Started for Free</h2>
-								<button
-									type="button"
-									className="btn btn-outline-dark login-btn"
-								>
-									<FacebookIcon
-										className="login-icon"
-										style={{ color: '#1976d2' }}
-									/>
-									<p>Log in with Facebook</p>
-								</button>
-								<button
-									type="button"
-									className="btn btn-outline-dark login-btn"
-								>
-									<GitHubIcon className="login-icon" />
-									<p>Log in with GitHub</p>
-								</button>
-								<button
-									type="button"
-									className="btn btn-outline-dark login-btn"
-								>
-									<LinkedInIcon
-										className="login-icon"
-										style={{ color: '#1976d2' }}
-									/>
-									<p>Log in with LinkedIn</p>
-								</button>
+								<div className="card-body py-0">
+									{/* Show smaller buttons for large screens */}
+									{SocialMediaButtons('w-75 d-none d-lg-block')}
+									{/* Show full buttons for small screens */}
+									{SocialMediaButtons('d-lg-none')}
+								</div>
 							</div>
 						</div>
 					</div>
