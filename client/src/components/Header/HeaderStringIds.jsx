1051bc186c6ec7d0e8a2edbb31ee1069c6c254d0
enthusiastdev121 <enthusiastdev121@gmail.com>
2020-12-23 21:06:50 -0600

    reverse (#62)

--- client/src/components/Header/HeaderStringIds.jsx
+++ client/src/components/Header/HeaderStringIds.jsx
@@ -1,6 +1,6 @@
 export const ENHANCE_YOUR_SKILLS = 'Enhance your skills here'
 export const HEADER_DESCRIPTION_1 =
-	'A place to help you gain a strong foundation in data structures and'
+	'A place to help you build a strong foundation in data structures and'
 export const HEADER_DESCRIPTION_2 =
 	'algorithms and prepare for technical interviews, all in one platform.'
 export const START_LEARNING_TEXT = 'Start Learning'
