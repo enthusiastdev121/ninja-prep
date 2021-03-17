24f5d3b13293c65f6ead7adf2b201bc81db7d78f
enthusiastdev121 <enthusiastdev121@gmail.com>
2021-03-17 17:05:57 -0400

    making responsive for tablet and mobile

--- client/src/components/Partials/NavigationBar/NavigationBarDesktop.jsx
+++ client/src/components/Partials/NavigationBar/NavigationBarDesktop.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Navbar, Nav, Image, Dropdown, Modal, Row, Col } from 'react-bootstrap'
+import { Navbar, Nav, Image, Dropdown, Modal, Row } from 'react-bootstrap'
 import LockIcon from '@material-ui/icons/Lock'
 import SettingsIcon from '@material-ui/icons/Settings'
 import BugReportIcon from '@material-ui/icons/BugReport'
@@ -12,6 +12,7 @@ import {
     CHALLENGES_NAVIGATION,
     MEET_THE_TEAM_NAVIGATION,
     SIGN_IN_NAVIGATION,
+    SIGN_UP_POPUP_BOX,
 } from './NavigationBarStringIds'
 import { FACEBOOK_LOGIN, GITHUB_LOGIN, GOOGLE_LOGIN, LOGIN_HEADER_TITLE, GET_STARTED } from '../../Login/LoginStringIds'
 
@@ -40,6 +41,7 @@ const SocialMediaButtons = (cssClass) => {
         </>
     )
 }
+
 function NavigationBarDesktop(props) {
     const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
         <a
@@ -105,10 +107,6 @@ function NavigationBarDesktop(props) {
             )
         } else {
             return (
-                // <Nav.Link as={Link} to="/login" className="pl-3 mr-5">
-                //     {SIGN_IN_NAVIGATION}
-                // </Nav.Link>
-
                 <Nav.Link as={Link} onClick={handleShortcutsShow} className="pl-3 mr-5">
                     {SIGN_IN_NAVIGATION}
                 </Nav.Link>
@@ -138,10 +136,12 @@ function NavigationBarDesktop(props) {
             </Navbar.Collapse>
 
             <Modal show={showShortcuts} onHide={handleShortcutsClose} centered>
-                {/* <Modal.Header closeButton><Modal.Title>Sign Up or Login</Modal.Title></Modal.Header> */}
-                <Modal.Body>
-                    <Row className="py-5 px-5 mx-auto h-100 text-center">
-                        <div className=" mx-auto px-auto center w-100">
+                <Modal.Header closeButton className="modal-header">
+                    <Modal.Title></Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="p-0">
+                    <Row className="pb-5 px-0 mx-auto h-100 text-center w-75">
+                        <div className=" mx-5 px-0 center w-100">
                             <img
                                 src="./images/NinjaPrep-Logo.png"
                                 className="logo"
@@ -149,8 +149,8 @@ function NavigationBarDesktop(props) {
                                 width="60"
                                 height="60"
                             />
-                            <h3 className="font-weight-bold py-4 mx-auto">Sign Up or Login to NinjaPrep</h3>
-                            <div>{SocialMediaButtons('mx-auto fluid ui mb-4 py-3 ')}</div>
+                            <h3 className="font-weight-bold py-4 mx-auto">{SIGN_UP_POPUP_BOX}</h3>
+                            <div>{SocialMediaButtons('mx-auto fluid ui mb-3 py-3')}</div>
                         </div>
                     </Row>
                 </Modal.Body>
