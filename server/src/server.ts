5bdc8165af97c1ef17c1d19bedb390489bd5f044
enthusiastdev121 <enthusiastdev121@gmail.com>
2021-04-24 19:33:52 -0700

    Adding update to build path for ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -24,10 +24,10 @@ if (process.env.NODE_ENV) {
 console.log(`production is ${inProduction}`)
 
 if (inProduction) {
-    app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
+    app.use(express.static(path.join(__dirname, '../../../', 'client', 'build')))
 
     app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, '../../../', 'client', 'build', 'index.html'))
     })
 }
 
